c689c7d946e4a8c4f226702a1a23b7c3f7a25c45
PrinceM <parames.pasitprachaja@gmail.com>
2022-12-23 12:39:46 +0700

    update: <InputTodo /> - import and use [validator] for input validation - use <form onSubmit> instead of onClick so it can add To-Do list with both click button and press enter

--- src/component/InputToDo.js
+++ src/component/InputToDo.js
@@ -1,16 +1,25 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
 
 const InputToDo = ({ items, setItems }) => {
-    const [newItem, setNewItem] = useState('');                 // for input data
+    const [newItem, setNewItem] = useState('');     // for input data
+    const [error, setError] = useState('');
 
-    // action when user click 'Add' button
-    const handleAddNewItem = () => {
-        const tempArr = [...items];
-        tempArr.unshift({ id: uuidv4(), title: newItem, completed: false });
-        setItems(tempArr);
-        setNewItem('');
-    };
+    // action when submit (both click on 'Add' and press 'Enter')
+    // ---> use [form + submit] instead of [onClick]
+    const handleSubmit = (e) => {
+        e.preventDefault();     // avoid refesh page after submitting
+        if (validator.isEmpty(newItem, { ignore_whitespace: true })) {
+            setError('To-Do List is required');
+        } else {
+            setError('');
+            const tempArr = [...items];
+            tempArr.unshift({ id: uuidv4(), title: newItem, completed: false });
+            setItems(tempArr);
+            setNewItem('');
+        }
+    }
 
     // action when user click 'Reset' button
     const handleResetNewItem = () => setNewItem('');
@@ -19,11 +28,28 @@ const InputToDo = ({ items, setItems }) => {
     const handleInputChange = (e) => setNewItem(e.target.value);
 
     return (
-        <div className="input-group">
-            <input className="form-control" value={newItem} onChange={handleInputChange} />
-            <button className="btn btn-primary" onClick={handleAddNewItem}><i className="fa-solid fa-check" /></button>
-            <button className="btn btn-outline-secondary" onClick={handleResetNewItem}><i className="fa-solid fa-xmark" /></button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="input-group">
+                <input
+                    className={`form-control ${error ? 'is-invalid' : ''}`}
+                    value={newItem} onChange={handleInputChange}
+                    placeholder='Enter your To-Do List'
+                />
+
+                <button className="btn btn-primary">
+                    <i className="fa-solid fa-check" />
+                </button>
+
+                <button
+                    type='button'
+                    className="btn btn-outline-secondary"
+                    onClick={handleResetNewItem}
+                >
+                    <i className="fa-solid fa-xmark" />
+                </button>
+            </div>
+            {error && <small className='text-danger'>{error}</small>}
+        </form>
     );
 }
 export default InputToDo;
