becd48451287ea4df8b5cb1c1f575023750aa5a9
PrinceM <parames.pasitprachaja@gmail.com>
2022-12-23 10:37:50 +0700

    update: set [completed] status using [handleClickUpdateItems] function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import InputToDo from './component/InputToDo';
 import ListItems from './component/ListItems';
-import { useState } from 'react';
 
 function App() {
   // defaul data
@@ -23,13 +23,14 @@ function App() {
     const newItems = items.filter(item => item.id !== idToDelete);
     setItems(newItems);
   }
-  // edit mode --- action when click 'Done' button
-  const handleClickUpdateItems = (idxToUpdate, editItem, handlerIsEdit) => {
-    const tempArr = [...items];                                                   // clone [items] array
-    const newObj = Object.assign({}, tempArr[idxToUpdate], { title: editItem })   // create new object with 'editItem'
-    tempArr.splice(idxToUpdate, 1, newObj);                                       // substitute tempArr[idxToUpdate] with {newObj}
-    setItems(tempArr);                                                            // update [items] with [tempArr]
-    handlerIsEdit();                                                              // toggle isEdit status <----------- function from <Item />
+  // edit mode --- action when click 'Done' or 'Change' button
+  const handleClickUpdateItems = (idxToUpdate, editItem, keyToUpdate, handlerIsEdit) => {
+    const tempArr = [...items];                                                               // clone [items] array
+    const newObj = Object.assign({}, tempArr[idxToUpdate], { [keyToUpdate]: editItem })       // create new object with 'editItem'
+    tempArr.splice(idxToUpdate, 1, newObj);                                                   // substitute tempArr[idxToUpdate] with {newObj}
+    setItems(tempArr);                                                                        // update [items] with [tempArr]
+
+    if (keyToUpdate === 'title') handlerIsEdit();                                             // toggle isEdit status <----------- function from <Item />
   }
   // --------------------------------------------------------------------------------------------
 
--- src/component/Item.js
+++ src/component/Item.js
@@ -3,11 +3,9 @@ import { useState } from "react";
 const Item = ({ idx, item, onClickDelete, onClickUpdateItem }) => {
     const [isEdit, setIsEdit] = useState(false);                        // for toggling between display and edit mode
     const [editItem, setEditItem] = useState(item.title);               // for edit data
-    const [isDoneItem, setIsDoneItem] = useState(item.completed);       // for toggling between green and red (click on change button)
 
-    // toggle between green and red (click on change button)
-    const handleIsDoneItem = () => setIsDoneItem(!isDoneItem);
-    const statusColor = isDoneItem ? '#14A44D' : '#DC4C64';
+    // set status color due to current 'completed' status
+    const statusColor = item.completed ? '#14A44D' : '#DC4C64';
 
     // toggle between display and edit mode
     // ----- 1. happen when click 'item' in display mode         <-- happen inside this component
@@ -26,14 +24,14 @@ const Item = ({ idx, item, onClickDelete, onClickUpdateItem }) => {
     // item: display mode
     const displayItemMode = <div className="input-group">
         <div className="form-control" onClick={handlerIsEdit} style={{ backgroundColor: statusColor, color: "white" }}>{item.title}</div>
-        <button className="btn btn-outline-secondary" onClick={handleIsDoneItem} >Change</button>
+        <button className="btn btn-outline-secondary" onClick={() => onClickUpdateItem(idx, !item.completed, 'completed', handlerIsEdit)} >Change</button>
         <button className="btn btn-outline-secondary" onClick={() => onClickDelete(item.id)} >Delete</button>
     </div>
 
     // item: edit mode
     const editItemMode = <div className="input-group">
         <input className="form-control" onChange={handleEditItem} value={editItem} />
-        <button className="btn btn-outline-secondary" onClick={() => onClickUpdateItem(idx, editItem, handlerIsEdit)} >Done</button>
+        <button className="btn btn-outline-secondary" onClick={() => onClickUpdateItem(idx, editItem, 'title', handlerIsEdit)} >Done</button>
         <button className="btn btn-outline-secondary" onClick={hadleCancelItem} >Cancel</button>
     </div>
 
